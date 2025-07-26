e7b512ab46509ad9a3040ffd8783fc43df4800fe
Suriya-8726 <2420030786@klh.edu.in>
2025-07-26 10:51:44 +0530

    Add files via upload

---
+++ xor.java
@@ -0,0 +1,18 @@
+package oops_practicles;
+import java.util.Scanner;
+
+public class xor {
+	public static void main(String args[]) {
+	      int a = 60;	
+	      int b = 13;	
+	      int c = 0;
+
+	      c = a ^ b;       
+	      System.out.println("a ^ b = " + c );
+
+	      c = ~a;           
+	      System.out.println("~a = " + c );
+	   }
+	}
+
+

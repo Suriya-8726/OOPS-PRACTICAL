f580c5f050128ad65fcd387b79696348790db8c3
Suriya-8726 <2420030786@klh.edu.in>
2025-07-26 10:48:22 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,19 @@
+package oops_practicles;
+import java.util.Scanner;
+
+public class bitwise {
+
+		   public static void main(String args[]) {
+		      int a = 60;	
+		      int b = 13;	
+		      int c = 0;
+
+		      c = a & b;       
+		      System.out.println("a & b = " + c );
+
+		      c = a | b;        
+		      System.out.println("a | b = " + c );
+		   }
+		}
+
+

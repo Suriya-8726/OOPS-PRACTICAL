e165010b31f4ae89f554d5eb2ac26fd1eee2441d
Suriya-8726 <2420030786@klh.edu.in>
2025-07-26 11:30:16 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,18 @@
+package oop;
+import java.util.Scanner;
+
+public class logical {
+		  public static void main(String[] args) {
+
+		    System.out.println((5 > 3) && (8 > 5));  
+		    System.out.println((5 > 3) && (8 < 5));  
+
+		    System.out.println((5 < 3) || (8 > 5));  
+		    System.out.println((5 > 3) || (8 < 5));  
+		    System.out.println((5 < 3) || (8 < 5));  
+
+		    System.out.println(!(5 == 3));  
+		    System.out.println(!(5 > 3));  
+		  }
+		}
+
---
+++ terinary.java
@@ -0,0 +1,14 @@
+package oops_practicles;
+import java.util.Scanner;
+
+public class terinary {
+		  public static void main(String[] args) {
+
+		    int februaryDays = 29;
+		    String result;
+
+		    result = (februaryDays == 28) ? "Not a leap year" : "Leap year";
+		    System.out.println(result);
+		  }
+		}
+

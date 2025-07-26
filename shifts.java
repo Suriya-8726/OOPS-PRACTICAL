13eea8ff92e48918b2b390b56f529d3660c4ecc4
Suriya-8726 <2420030786@klh.edu.in>
2025-07-26 10:55:01 +0530

    Add files via upload

---
+++ shifts.java
@@ -0,0 +1,18 @@
+package oops_practicles;
+import java.util.Scanner;
+
+public class shifts {
+	public static void main(String args[]) {
+	      int a = 60;	
+	      int c = 0;
+
+	      c = a << 2;       
+	      System.out.println("a << 2 = " + c );
+
+	      c = a >> 2;      
+	      System.out.println("a >> 2  = " + c );
+
+	      c = a >>> 2;      
+	      System.out.println("a >>> 2 = " + c );
+	   }
+}

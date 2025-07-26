27a2e6c0f3e303da365fc0d0beeaf35f7009c67b
Suriya-8726 <2420030786@klh.edu.in>
2025-07-26 10:33:44 +0530

    Add files via upload

---
+++ arthematicop.java
@@ -0,0 +1,22 @@
+package oops_practicles;
+import java.util.Scanner;
+
+
+public class arthematicop {
+		  public static void main(String[] args) {
+			  int a=10,b=12;
+			  
+		    
+		    System.out.println("a + b = " + (a + b));
+
+		    System.out.println("a - b = " + (a - b));
+
+		    System.out.println("a * b = " + (a * b));
+
+		    System.out.println("a / b = " + (a / b));
+
+		    System.out.println("a % b = " + (a % b));
+		  }
+		}
+
+
